282d33cd15e1eafa1d0c591cab91f7ff67a7cde8
aartijangra <aartijangra482@gmail.com>
2024-04-22 17:30:59 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const jobRoutes = require('./routes/jobs.route');
+const cors = require("cors")
 
 const app = express();
 
 // Body parser middleware
 app.use(bodyParser.json());
+app.use(cors(
+    {
+        origin: "*"
+    }
+))
 
 // MongoDB Configuration
 const db = process.env.MONGO_URI
 
 
 mongoose
-  .connect(db)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+    .connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
 // Routes
 app.use('/api/jobs', jobRoutes);
